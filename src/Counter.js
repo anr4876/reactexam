86934cfac178f69434806c32cac6d524e04f4e23
anr4876 <dustjdanr@naver.com>
2022-07-03 10:32:38 +0900

    reactexam1

--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,10 @@
+import React,(useState) from "react";
+
 const Counter = () => {
     return (
         <div>
             <h2>0</h2>
+            <h3>삭제</h3>
             <button>+</button>
             <button>-</button>
         </div>
